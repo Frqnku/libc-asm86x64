be079b8c5d3bf11b88fd9f4594dde3600e6e1f1a
Frqnku <ugo.tiberto@gmail.com>
2025-08-08 16:59:09 +0200

    fix: added a push of rax to the stack to keep  the value before calling __erno_location

--- asm/ft_read.s
+++ asm/ft_read.s
@@ -1,21 +1,24 @@
 extern __errno_location
 
 section .text
-	global ft_read ;
+    global ft_read ;
 
 ft_read:
-	mov rax, 0
-	syscall
+    mov rax, 0
+    syscall
 
-	cmp rax, 0
+    cmp rax, 0
     jge .done
 
     neg rax
+    push rax
 
     call __errno_location wrt ..plt
-    mov [rax], eax
+    pop rcx
+    mov [rax], ecx
 
     mov rax, -1
+    ret
 
 .done:
-	ret
+    ret
